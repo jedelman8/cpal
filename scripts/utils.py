5896a0d65fc45de3be92ef07f5fdfacdf99082b9
Yandy Ramirez <yandyr@gmail.com>
2014-04-05 15:36:57 -0400

    renamed suffixes to KB, GB and so on

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,8 @@ SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB'],
             1024: ['KB', 'MB', 'GB', 'TB']}
 
 def convertSize(size, suffix, kilobyte_1024_bytes=True):
-    '''Convert a file size to human-readable form.
+    '''
+    Convert a file size to human-readable form.
 
     Keyword arguments:
     size                                     --      file size in bytes
